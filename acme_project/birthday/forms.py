2ec2d47e7eff5f5e4325b5cb139b420de0885294
Andrew Sokolov <ya.17bit@ya.ru>
2025-05-29 17:49:09 +0300

    Add Forms

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,7 +1,9 @@
 # birthday/forms.py
 from django import forms
+# Импортируем класс модели Birthday.
+from .models import Birthday
 
-
+'''
 class BirthdayForm(forms.Form):
     # Максимальная длина
     first_name = forms.CharField(max_length=20, label='Имя')
@@ -13,3 +15,16 @@ class BirthdayForm(forms.Form):
         # Указываем, что виджет для ввода даты должен быть с типом date.
         widget=forms.DateInput(attrs={'type': 'date'})
         )
+'''
+
+
+class BirthdayForm(forms.ModelForm):
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
